555c0d2c54c07318a9ef27b56c7d0908bad2e45d
Gustavo Vidal de Freitas <gustavo2892@gmail.com>
2025-09-10 14:01:01 -0300

    Create badge component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import Text from "./components/text";
+import Badge from "./components/badge";
+import Icon from "./components/icon";
 
 import TrashIcon from "./assets/icons/trash.svg?react";
 import CheckIcon from "./assets/icons/check.svg?react";
@@ -6,7 +8,6 @@ import XIcon from "./assets/icons/x.svg?react";
 import PencilIcon from "./assets/icons/pencil.svg?react";
 import PlusIcon from "./assets/icons/plus.svg?react";
 import SpinnerIcon from "./assets/icons/spinner.svg?react";
-import Icon from "./components/icon";
 
 export default function App() {
   return (
@@ -27,6 +28,11 @@ export default function App() {
         <Icon svg={SpinnerIcon} />
         <Icon svg={PencilIcon} />
         <Icon svg={XIcon} />
+      </div>
+
+      <div>
+        <Badge variant="secondary">5</Badge>
+        <Badge variant="primary">2 de 5</Badge>
       </div>
     </div>
   )
