bd7c11170580a3ce2524aa8c5081df567edee8fc
Gustavo Vidal de Freitas <gustavo2892@gmail.com>
2025-09-14 11:49:12 -0300

    Update task in local storage

--- src/core-components/task-item.tsx
+++ src/core-components/task-item.tsx
@@ -11,6 +11,7 @@ import XIcon from "../assets/icons/x.svg?react";
 import CheckIcon from "../assets/icons/check.svg?react";
 import InputText from "../components/input-text";
 import { Task, TaskState } from "../models/task";
+import useTask from "../hooks/use-task";
 
 interface TaskItemProps {
   task: Task;
@@ -21,7 +22,8 @@ export default function TaskItem({ task }: TaskItemProps) {
     task?.state === TaskState.Creating
   );
 
-  const [taskTitle, setTaskTitle] = React.useState("");
+  const [taskTitle, setTaskTitle] = React.useState(task.title || "");
+  const { updateTask } = useTask();
 
   function handleEditTask() {
     setIsEditing(true);
@@ -37,8 +39,7 @@ export default function TaskItem({ task }: TaskItemProps) {
 
   function handleSaveTask(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    console.log({ id: task.id, title: taskTitle });
-    // chamada para a função de atualizar;
+    updateTask(task.id, { title: taskTitle });
     setIsEditing(false);
   }
 
@@ -65,6 +66,7 @@ export default function TaskItem({ task }: TaskItemProps) {
       ) : (
         <form onSubmit={handleSaveTask} className="flex items-center gap-4">
           <InputText
+            value={taskTitle}
             className="flex-1"
             onChange={handleChangeTaskTitle}
             required
